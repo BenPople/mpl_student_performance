96dbfc2cf40df6eebcbdcff1f724dd8240371877
Ben Pople <popleb@btc.ac.uk>
2021-12-07 16:38:45 +0000

    c2

--- sperf.py
+++ sperf.py
@@ -1,11 +1,26 @@
 import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
 
 dataframe = pd.read_csv("StudentsPerformance.csv")
 
-x = dataframe['reading score']
-y = dataframe['math score']
+x = dataframe['gender']
+y = dataframe.groupby('gender').get_group('female')['math score'].mean()
+z = dataframe.groupby('gender').get_group('male')['math score'].mean()
 
-plt.scatter(x, y)
+a = dataframe['lunch']
+b = dataframe.groupby('lunch').get_group('standard')['math score'].mean()
+c = dataframe.groupby('lunch').get_group('free/reduced')['math score'].mean()
+
+plt.subplot(1, 2, 1)
+plt.xlabel("Gender")
+plt.ylabel("Math Score")
+
+plt.bar(x.unique(), [z, y])
+
+plt.subplot(1, 2, 2)
+plt.xlabel("Lunch Type")
+plt.ylabel("Math Score")
+
+plt.bar(a.unique(), [b, c])
+
 plt.show()
